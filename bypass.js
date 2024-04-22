343071d7c6c093c0d52c807593d4118b628f5b28
coodeex <juhani.s.lehto@gmail.com>
2024-04-23 00:21:05 +0200

    ensure hostname

--- bypass.js
+++ bypass.js
@@ -1,10 +1,12 @@
+// Check if the current domain is google.com
+if (window.location.hostname !== "www.google.com") {
+    return;
+}
+
 // Find the button with the specified ID
 const rejectButton = document.getElementById("W0wltc");
 
 // Check if the button has the expected text and click it
 if (rejectButton && rejectButton.innerText.toLowerCase().includes("reject all")) {
     rejectButton.click();
-} else {
-    // Show an alert if the button is not found or doesn't contain the expected text
-    window.alert("Reject all button not found");
 }
